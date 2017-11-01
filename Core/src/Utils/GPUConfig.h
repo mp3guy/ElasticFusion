6903c0514fb0d7a6974cbb1688195302e0498cd6
Geoff <p0wdrdotcom@users.noreply.github.com>
2017-11-01 20:53:24 +1300

    Update GPUConfig.h for GeForce GTX 965M
    Update GPUConfig.h for GeForce GTX 965M

--- Core/src/Utils/GPUConfig.h
+++ Core/src/Utils/GPUConfig.h
@@ -84,6 +84,11 @@ class GPUConfig
             rgbStepMap["GeForce GTX 970"] = std::pair<int, int>(160, 64);
             rgbResMap["GeForce GTX 970"] = std::pair<int, int>(128, 272);
             so3StepMap["GeForce GTX 970"] = std::pair<int, int>(96, 64);
+               
+            icpStepMap["GeForce GTX 965M"] = std::pair<int, int>(256, 32);
+            rgbStepMap["GeForce GTX 965M"] = std::pair<int, int>(224, 16);
+            rgbResMap["GeForce GTX 965M"] = std::pair<int, int>(384, 480);
+            so3StepMap["GeForce GTX 965M"] = std::pair<int, int>(160, 32);
 
             icpStepMap["GeForce GTX 675MX"] = std::pair<int, int>(128, 80);
             rgbStepMap["GeForce GTX 675MX"] = std::pair<int, int>(128, 48);

5bc57565f9f8402054cb2019b3df5695b328c78a
Daichou <tommy0705c@gmail.com>
2018-10-07 18:38:30 +0800

    rs2: build.sh add ubuntu 18.04 support

--- build.sh
+++ build.sh
@@ -4,30 +4,38 @@ mkdir deps &> /dev/null
 cd deps
 
 #Add necessary extra repos
-#version=$(lsb_release -a 2>&1)
-#if [[ $version == *"14.04"* ]] ; then
-#    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-#    sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-#    rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
-#    sudo apt-get update
-#    sudo apt-get install cuda-7-5
-#elif [[ $version == *"15.04"* ]] ; then
-#    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1504/x86_64/cuda-repo-ubuntu1504_7.5-18_amd64.deb
-#    sudo dpkg -i cuda-repo-ubuntu1504_7.5-18_amd64.deb
-#    rm cuda-repo-ubuntu1504_7.5-18_amd64.deb
-#    sudo apt-get update
-#    sudo apt-get install cuda-7-5
-#elif [[ $version == *"16.04"* ]] ; then
-#    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-#    sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-#    rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-#    sudo add-apt-repository ppa:openjdk-r/ppa 
-#    sudo apt-get update
-#    sudo apt-get install cuda-8-0
-#else
-#    echo "Don't use this on anything except 14.04, 15.04, or 16.04"
-#    exit
-#fi
+version=$(lsb_release -a 2>&1)
+if [[ $version == *"14.04"* ]] ; then
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    sudo apt-get update
+    sudo apt-get install cuda-7-5
+elif [[ $version == *"15.04"* ]] ; then
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1504/x86_64/cuda-repo-ubuntu1504_7.5-18_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1504_7.5-18_amd64.deb
+    rm cuda-repo-ubuntu1504_7.5-18_amd64.deb
+    sudo apt-get update
+    sudo apt-get install cuda-7-5
+elif [[ $version == *"16.04"* ]] ; then
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    rm cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+    sudo add-apt-repository ppa:openjdk-r/ppa
+    sudo apt-get update
+    sudo apt-get install cuda-8-0
+elif [[ $version == *"18.04"* ]] ; then
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    rm cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+    sudo add-apt-repository ppa:openjdk-r/ppa
+    sudo apt-get update
+    sudo apt-get install cuda
+else
+    echo "Don't use this on anything except 14.04, 15.04, 16.04 or 18.04"
+    exit
+fi
 
 sudo apt-get install -y cmake-qt-gui git build-essential libusb-1.0-0-dev libudev-dev openjdk-7-jdk freeglut3-dev libglew-dev libsuitesparse-dev libeigen3-dev zlib1g-dev libjpeg-dev
 

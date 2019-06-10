ff7750961bb9c968a34a691a0e0a3615f0fb9813
Hongtao Wu <hwu67@jhu.edu>
2019-06-10 17:08:39 -0400

    Update for CUDA 9.0 or later

--- Core/src/Cuda/reduce.cu
+++ Core/src/Cuda/reduce.cu
@@ -55,7 +55,7 @@
 
 #if __CUDA_ARCH__ < 300
 __inline__ __device__
-float __shfl_down(float val, int offset, int width = 32)
+float __shfl_down_sync(unsigned mask, float val, int offset, int width = 32)
 {
     static __shared__ float shared[MAX_THREADS];
     int lane = threadIdx.x % 32;
@@ -67,7 +67,7 @@ float __shfl_down(float val, int offset, int width = 32)
 }
 
 __inline__ __device__
-int __shfl_down(int val, int offset, int width = 32)
+int __shfl_down_sync(unsigned mask, int val, int offset, int width = 32)
 {
     static __shared__ int shared[MAX_THREADS];
     int lane = threadIdx.x % 32;
@@ -91,41 +91,41 @@ __inline__  __device__ JtJJtrSE3 warpReduceSum(JtJJtrSE3 val)
 {
     for(int offset = warpSize / 2; offset > 0; offset /= 2)
     {
-        val.aa += __shfl_down(val.aa, offset);
-        val.ab += __shfl_down(val.ab, offset);
-        val.ac += __shfl_down(val.ac, offset);
-        val.ad += __shfl_down(val.ad, offset);
-        val.ae += __shfl_down(val.ae, offset);
-        val.af += __shfl_down(val.af, offset);
-        val.ag += __shfl_down(val.ag, offset);
+        val.aa += __shfl_down_sync(0xFFFFFFFF, val.aa, offset);
+        val.ab += __shfl_down_sync(0xFFFFFFFF, val.ab, offset);
+        val.ac += __shfl_down_sync(0xFFFFFFFF, val.ac, offset);
+        val.ad += __shfl_down_sync(0xFFFFFFFF, val.ad, offset);
+        val.ae += __shfl_down_sync(0xFFFFFFFF, val.ae, offset);
+        val.af += __shfl_down_sync(0xFFFFFFFF, val.af, offset);
+        val.ag += __shfl_down_sync(0xFFFFFFFF, val.ag, offset);
 
-        val.bb += __shfl_down(val.bb, offset);
-        val.bc += __shfl_down(val.bc, offset);
-        val.bd += __shfl_down(val.bd, offset);
-        val.be += __shfl_down(val.be, offset);
-        val.bf += __shfl_down(val.bf, offset);
-        val.bg += __shfl_down(val.bg, offset);
+        val.bb += __shfl_down_sync(0xFFFFFFFF, val.bb, offset);
+        val.bc += __shfl_down_sync(0xFFFFFFFF, val.bc, offset);
+        val.bd += __shfl_down_sync(0xFFFFFFFF, val.bd, offset);
+        val.be += __shfl_down_sync(0xFFFFFFFF, val.be, offset);
+        val.bf += __shfl_down_sync(0xFFFFFFFF, val.bf, offset);
+        val.bg += __shfl_down_sync(0xFFFFFFFF, val.bg, offset);
 
-        val.cc += __shfl_down(val.cc, offset);
-        val.cd += __shfl_down(val.cd, offset);
-        val.ce += __shfl_down(val.ce, offset);
-        val.cf += __shfl_down(val.cf, offset);
-        val.cg += __shfl_down(val.cg, offset);
+        val.cc += __shfl_down_sync(0xFFFFFFFF, val.cc, offset);
+        val.cd += __shfl_down_sync(0xFFFFFFFF, val.cd, offset);
+        val.ce += __shfl_down_sync(0xFFFFFFFF, val.ce, offset);
+        val.cf += __shfl_down_sync(0xFFFFFFFF, val.cf, offset);
+        val.cg += __shfl_down_sync(0xFFFFFFFF, val.cg, offset);
 
-        val.dd += __shfl_down(val.dd, offset);
-        val.de += __shfl_down(val.de, offset);
-        val.df += __shfl_down(val.df, offset);
-        val.dg += __shfl_down(val.dg, offset);
+        val.dd += __shfl_down_sync(0xFFFFFFFF, val.dd, offset);
+        val.de += __shfl_down_sync(0xFFFFFFFF, val.de, offset);
+        val.df += __shfl_down_sync(0xFFFFFFFF, val.df, offset);
+        val.dg += __shfl_down_sync(0xFFFFFFFF, val.dg, offset);
 
-        val.ee += __shfl_down(val.ee, offset);
-        val.ef += __shfl_down(val.ef, offset);
-        val.eg += __shfl_down(val.eg, offset);
+        val.ee += __shfl_down_sync(0xFFFFFFFF, val.ee, offset);
+        val.ef += __shfl_down_sync(0xFFFFFFFF, val.ef, offset);
+        val.eg += __shfl_down_sync(0xFFFFFFFF, val.eg, offset);
 
-        val.ff += __shfl_down(val.ff, offset);
-        val.fg += __shfl_down(val.fg, offset);
+        val.ff += __shfl_down_sync(0xFFFFFFFF, val.ff, offset);
+        val.fg += __shfl_down_sync(0xFFFFFFFF, val.fg, offset);
 
-        val.residual += __shfl_down(val.residual, offset);
-        val.inliers += __shfl_down(val.inliers, offset);
+        val.residual += __shfl_down_sync(0xFFFFFFFF, val.residual, offset);
+        val.inliers += __shfl_down_sync(0xFFFFFFFF, val.inliers, offset);
     }
 
     return val;
@@ -188,20 +188,20 @@ __inline__  __device__ JtJJtrSO3 warpReduceSum(JtJJtrSO3 val)
 {
     for(int offset = warpSize / 2; offset > 0; offset /= 2)
     {
-        val.aa += __shfl_down(val.aa, offset);
-        val.ab += __shfl_down(val.ab, offset);
-        val.ac += __shfl_down(val.ac, offset);
-        val.ad += __shfl_down(val.ad, offset);
+        val.aa += __shfl_down_sync(0xFFFFFFFF, val.aa, offset);
+        val.ab += __shfl_down_sync(0xFFFFFFFF, val.ab, offset);
+        val.ac += __shfl_down_sync(0xFFFFFFFF, val.ac, offset);
+        val.ad += __shfl_down_sync(0xFFFFFFFF, val.ad, offset);
 
-        val.bb += __shfl_down(val.bb, offset);
-        val.bc += __shfl_down(val.bc, offset);
-        val.bd += __shfl_down(val.bd, offset);
+        val.bb += __shfl_down_sync(0xFFFFFFFF, val.bb, offset);
+        val.bc += __shfl_down_sync(0xFFFFFFFF, val.bc, offset);
+        val.bd += __shfl_down_sync(0xFFFFFFFF, val.bd, offset);
 
-        val.cc += __shfl_down(val.cc, offset);
-        val.cd += __shfl_down(val.cd, offset);
+        val.cc += __shfl_down_sync(0xFFFFFFFF, val.cc, offset);
+        val.cd += __shfl_down_sync(0xFFFFFFFF, val.cd, offset);
 
-        val.residual += __shfl_down(val.residual, offset);
-        val.inliers += __shfl_down(val.inliers, offset);
+        val.residual += __shfl_down_sync(0xFFFFFFFF, val.residual, offset);
+        val.inliers += __shfl_down_sync(0xFFFFFFFF, val.inliers, offset);
     }
 
     return val;
@@ -681,8 +681,8 @@ __inline__  __device__ int2 warpReduceSum(int2 val)
 {
     for(int offset = warpSize / 2; offset > 0; offset /= 2)
     {
-        val.x += __shfl_down(val.x, offset);
-        val.y += __shfl_down(val.y, offset);
+        val.x += __shfl_down_sync(0xFFFFFFFF, val.x, offset);
+        val.y += __shfl_down_sync(0xFFFFFFFF, val.y, offset);
     }
 
     return val;
